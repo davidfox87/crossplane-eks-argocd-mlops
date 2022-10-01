7c132aa07999583ade3d79f674acadc0f907aaab
david.fox <david.fox@albeado.com>
2022-10-01 16:18:09 -0700

    increased replicas

--- applications/loadgen.sh
+++ applications/loadgen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while true; do curl -s -o /dev/null "http://172.17.0.2:31147/task-tracker-app-ui"; done
+while true; do curl -s -o /dev/null "http://172.17.0.2:32097/task-tracker-app-ui"; done
