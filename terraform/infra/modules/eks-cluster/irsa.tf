d3b66e61b975fa91bf86b365ccd6da43eb504fd1
david.fox <david.fox@albeado.com>
2022-10-02 14:47:26 -0700

    updates to terraform scripts

--- terraform/infra/modules/eks-cluster/irsa.tf
+++ terraform/infra/modules/eks-cluster/irsa.tf
@@ -95,7 +95,7 @@ data "aws_iam_policy_document" "s3-access" {
                 "s3:ListBucket",
                 "s3:GetBucketLocation"
     ]
-    resources = [ "arn:aws:s3::::*" ]
+    resources = [ "arn:aws:s3:::artifact-repository" ]
   }
 }
 
