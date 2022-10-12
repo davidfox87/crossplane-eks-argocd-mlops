2cd2274de192658914d165cbc2f067fb22737d39
david.fox <david.fox@albeado.com>
2022-10-11 22:33:54 -0700

    using flexible kustomize deployment of mlflow that can deploy locally and in production

---
+++ applications/mlflow/docker/build-and-push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+docker login --username=foxy7887 -p MGSGiw550!
+
+image_name=foxy7887/mlflow
+image_tag=v1
+full_image_name=${image_name}:${image_tag}
+
+cd "$(dirname "$0")" 
+docker build -t "${full_image_name}" .
+docker push "$full_image_name"
+
+# Output the strict image name, which contains the sha256 image digest
+docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
