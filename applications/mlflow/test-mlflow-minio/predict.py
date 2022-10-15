5492b8cfb78f9868d9fa76cf47408a40aa2bd939
david.fox <david.fox@albeado.com>
2022-10-14 23:08:11 -0700

    IT WORKS!!!!!! MLOPS

---
+++ applications/mlflow/test-mlflow-minio/predict.py
@@ -0,0 +1,44 @@
+from argparse import ArgumentParser
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+import mlflow
+import mlflow.xgboost
+
+print("Tracking URI:", mlflow.tracking.get_tracking_uri())
+print("MLflow Version:", mlflow.__version__)
+print("XGBoost version:",xgb.__version__)
+
+def build_data(data_path):
+    data = pd.read_csv(data_path)
+    X = data.drop(["quality"], axis=1)
+    y = data["quality"]
+    return X, y
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model_uri", dest="model_uri", help="model_uri", required=True)
+    parser.add_argument("--data_path", dest="data_path", help="data_path", default="data/wine-quality-red.csv")
+    args = parser.parse_args()
+    print("Arguments:")
+    for arg in vars(args):
+        print(f"  {arg}: {getattr(args, arg)}")
+
+    X,y  = build_data(args.data_path)
+    X_xgb = xgb.DMatrix(X, label=y)
+        
+    print("\n=== mlflow.xgboost.load_model")
+    model = mlflow.xgboost.load_model(args.model_uri)
+    print("model:", model)
+    predictions = model.predict(X_xgb)
+    print("predictions.type:", type(predictions))
+    print("predictions.shape:", predictions.shape)
+    print("predictions:", predictions)
+
+    print("\n=== mlflow.pyfunc.load_model")
+    model = mlflow.pyfunc.load_model(args.model_uri)
+    print("model:", model)
+    predictions = model.predict(X)
+    print("predictions.type:", type(predictions))
+    print("predictions.shape:", predictions.shape)
+    print("predictions:", predictions)
