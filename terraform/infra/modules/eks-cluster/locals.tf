6cacfc7288490f0ea0af38c4cb6828c1166bf4b8
david.fox <david.fox@albeado.com>
2022-11-05 17:42:21 -0700

    added ci clusterworkflowtemplate

--- terraform/infra/modules/eks-cluster/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  k8s_service_account_name      = "aws-load-balancer-controller"
-  k8s_service_account_namespace = "kube-system"
-
-  k8s_service_account_name_s3access      = "s3-artifact-storage"
-  k8s_service_account_namespace_mlflow = "mlflow"
-}
