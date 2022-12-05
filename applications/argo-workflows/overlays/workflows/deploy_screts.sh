61c8e5a087fcbcb8ff6c01d96a46ca26e28aafff
David Fox <davidmfox87@gmail.com>
2022-12-05 13:10:12 -0700

    deploy sealed secrets

--- applications/argo-workflows/overlays/workflows/deploy_screts.sh
+++ applications/argo-workflows/overlays/workflows/deploy_screts.sh
@@ -2,14 +2,14 @@
 
 kubectl create secret generic git-ssh -n workflows \
                 --dry-run=client \
-                --from-file=key=/home/david/.ssh/id_ed25519  -o yaml \
+                --from-file=key=/Users/david.fox/.ssh/id_ed25519  -o yaml \
                 | kubeseal  --controller-namespace kubeseal \
                             --controller-name sealed-secrets \
                             --format yaml > git-ssh-sealedsecret.yaml
 
 kubectl create secret generic git-known-hosts -n workflows \
                 --dry-run=client \
-                --from-file=ssh_known_hosts=/home/david/.ssh/known_hosts -o yaml \
+                --from-file=ssh_known_hosts=/Users/david.fox/.ssh/known_hosts -o yaml \
                 | kubeseal  --controller-namespace kubeseal \
                             --controller-name sealed-secrets \
                             --format yaml > git-known-hosts-sealedsecret.yaml
@@ -25,17 +25,17 @@ kubectl create secret generic argo-artifacts -n workflows \
 
 
 # write sealedsecret so that argo-cd can manage secrets from github
-kubectl create secret generic github-access -n workflows \
-                --dry-run=client \
-                --from-file=token=.env -o yaml \
-                | kubeseal  --controller-namespace kubeseal \
-                            --controller-name sealed-secrets \
-                            --format yaml > github-access-sealedsecret.yaml
+# kubectl create secret generic github-access -n workflows \
+#                 --dry-run=client \
+#                 --from-file=token=.env -o yaml \
+#                 | kubeseal  --controller-namespace kubeseal \
+#                             --controller-name sealed-secrets \
+#                             --format yaml > github-access-sealedsecret.yaml
 
 
 kubectl create secret generic regcred -n workflows \
                 --dry-run=client \
-                --from-file=.dockerconfigjson=/home/david/.docker/config.json \
+                --from-file=.dockerconfigjson=/Users/david.fox/.docker/config.json \
                 --type=kubernetes.io/dockerconfigjson -o yaml \
                 | kubeseal  --controller-namespace kubeseal \
                             --controller-name sealed-secrets \
